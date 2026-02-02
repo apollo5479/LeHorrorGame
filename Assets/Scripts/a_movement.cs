a4e283a29de50b3cc0c7feb1851414fabcc53368
Anooneee <72642342+Anooneee@users.noreply.github.com>
2026-02-01 21:29:45 -0700

    player stuff
    i put a_ in front of my scences and srcipts . a for Andrea

---
+++ Assets/Scripts/a_movement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class a_movement : MonoBehaviour
+{
+
+    Rigidbody body;
+    public float forward_force = 5;
+    public float backward_force = -3;
+    public Vector3 rotation_velocity = new Vector3(0, 25, 0);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        body.linearDamping = 5;
+        body.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(transform.forward * forward_force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(transform.forward * backward_force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(rotation_velocity * Time.fixedDeltaTime);
+            body.MoveRotation(body.rotation * deltaRotation);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion deltaRotation = Quaternion.Euler((-1) * rotation_velocity * Time.fixedDeltaTime);
+            body.MoveRotation(body.rotation * deltaRotation);
+        }
+    }
+}
